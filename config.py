2866d7d54f687817bbfb101379b52112ff3c09d1
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2024-10-28 21:35:54 +0900

    Added DSSR, RNAVIEW and RNAVIEW_PATH

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
-RNAVIEW=""
-DSSR=""
+RNAVIEW_PATH = "/large/otgk/app/rnaview/20231226/bin"
+# RNAVIEW="/large/otgk/app/rnaview/20231226"
+RNAVIEW = RNAVIEW_PATH.replace("/bin", "")
+
+DSSR_PATH = "OPTIONAL"
+
+
+WORK_DIR = "/large/otgk/app/PseudoknotVisualizer"
+INTEREMEDIATE_DIR = WORK_DIR + "/intermediate"
+
+
+
+
+
 
