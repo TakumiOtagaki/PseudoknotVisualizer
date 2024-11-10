25663f0016d0de950d3180148a196ce8dc8454dd
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2024-11-10 20:34:10 +0900

    input sentence modified

--- fetch_pdb.py
+++ fetch_pdb.py
@@ -22,7 +22,9 @@ def download_pdb(pdb_id, filename=None):
         print(f"Failed to download PDB file for {pdb_id}: {e}")
 
 # 使用例
-pdb_id = input("Enter PDB ID: ")
+pdb_id = input("Enter PDB ID (q to quit): ")
+if pdb_id == "q":
+    exit()
 output_file = input("Enter output filename(if not provided, pdb_id.pdb will be created in current directory): ")
 if output_file == "":
     output_file = "./" + pdb_id + ".pdb"
