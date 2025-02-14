7583e7fb5d1eb6a4cdbd995d708e34d8fdc4aa57
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2025-02-14 14:56:29 +0900

    deleted

--- config_mdx.py
+++
@@ -1,7 +0,0 @@
-# RNAVIEW related variables
-RNAVIEW="/large/otgk/app/rnaview/20231226"
-RNAVIEW_PATH="/large/otgk/app/rnaview/20231226/bin"
-# The path to this PseudoknotVisualizer repository.
-PseudoKnotVisualizer_DIR = "/large/otgk/PseudoknotVisualizer"
-# ------------- Do not edit below this line. ------------------
-INTEREMEDIATE_DIR = PseudoKnotVisualizer_DIR / "intermediate"
