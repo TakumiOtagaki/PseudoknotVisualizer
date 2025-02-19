c0ca850803db6eab7d1b3e5845a7f3bf6a88c71e
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2025-02-19 18:39:47 +0900

    added

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 
-# RNAVIEW related paths
 """
 The variable RNAVIEW is the path to the RNAView directory and RNAVIEW/bin/rnaview is the path to the RNAView executable.
 """
-RNAVIEW = Path("/Users/ootagakitakumi/Applications/RNAView") # <-- please edit this path to your RNAVIEW path
+
+# RNAVIEW related paths
+RNAVIEW = Path("/path/to/RNAView") # <-- please edit this path to your RNAVIEW path
 RNAVIEW_PATH = RNAVIEW / "bin"
 
 # PseudoknotVisualizer repository path
-PseudoKnotVisualizer_DIR = Path("/Users/ootagakitakumi/Library/Mobile Documents/com~apple~CloudDocs/大学院/浅井研究室/研究/PseudoknotVisualizer") # <-- please edit this path to your PseudoKnotVisualizer repository path
+PseudoKnotVisualizer_DIR = Path("/path/to/PseudoknotVisualizer") # <-- please edit this path to your PseudoKnotVisualizer repository path
 
 # ------------- Do not edit below this line. ------------------
 INTEREMEDIATE_DIR = PseudoKnotVisualizer_DIR / "intermediate"
