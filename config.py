fa3ec1cf45e4203cbc9c33d7f3574c18b54d0def
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2025-02-26 12:15:23 +0900

    __file__.parent

--- config.py
+++ config.py
@@ -1,15 +1,10 @@
 from pathlib import Path
 
-"""
-The variable RNAVIEW is the path to the RNAView directory and RNAVIEW/bin/rnaview is the path to the RNAView executable.
-"""
+# The variable RNAVIEW is the path to the RNAView directory and RNAVIEW/bin/rnaview is the path to the RNAView executable.
+RNAVIEW = Path("/dir/to/RNAView") # <-- please edit this path to your RNAVIEW directory.
 
-# RNAVIEW related paths
-RNAVIEW = Path("/path/to/RNAView") # <-- please edit this path to your RNAVIEW path
-RNAVIEW_PATH = RNAVIEW / "bin"
-
-# PseudoknotVisualizer repository path
-PseudoKnotVisualizer_DIR = Path("/path/to/PseudoknotVisualizer") # <-- please edit this path to your PseudoKnotVisualizer repository path
 
 # ------------- Do not edit below this line. ------------------
-INTEREMEDIATE_DIR = PseudoKnotVisualizer_DIR / "intermediate"
+RNAVIEW_PATH = RNAVIEW / "bin"
+PseudoKnotVisualizer_DIR = Path(__file__).parent
+INTERMEDIATE_DIR = PseudoKnotVisualizer_DIR / "intermediate"
